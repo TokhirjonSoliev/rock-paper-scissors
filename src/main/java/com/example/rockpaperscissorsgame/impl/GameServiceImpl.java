7d37605f9387401e13aa91d163471b33ca4a1483
tokhirjon <tokhirjon007@gmail.com>
2023-01-19 10:11:16 +0500

    connect to given API

--- src/main/java/com/example/rockpaperscissorsgame/impl/GameServiceImpl.java
+++ src/main/java/com/example/rockpaperscissorsgame/impl/GameServiceImpl.java
@@ -1,7 +1,12 @@
 package com.example.rockpaperscissorsgame.impl;
 
 import com.example.rockpaperscissorsgame.dto.ResponseDto;
+import com.example.rockpaperscissorsgame.dto.ResultDto;
 import com.example.rockpaperscissorsgame.service.GameService;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,5 +14,15 @@ public class GameServiceImpl implements GameService {
     @Override
     public ResponseDto checkResult(String choice) {
         return null;
+    }
+
+    public String getRandomChoice() {
+        Client client = ClientBuilder.newClient();
+        Response response = client.target("https://private-anon-da72517da4-curbrockpaperscissors.apiary-mock.com/rps-stage/throw")
+                .request(MediaType.TEXT_PLAIN_TYPE)
+                .get();
+
+        ResultDto result = response.readEntity(ResultDto.class);
+        return result.getBody();
     }
 }
