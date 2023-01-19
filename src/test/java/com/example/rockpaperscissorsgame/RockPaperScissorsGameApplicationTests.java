9d787e4e62ec06401ba72a02078995ed46b929fa
tokhirjon <tokhirjon007@gmail.com>
2023-01-19 10:03:41 +0500

    project init

---
+++ src/test/java/com/example/rockpaperscissorsgame/RockPaperScissorsGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rockpaperscissorsgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RockPaperScissorsGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
